570c4f5598d961b93007b40bf588e02c6c9ff7df
TejasNayak42 <tejasnayak.c42@gmail.com>
2023-06-07 23:19:00 +0530

    blank commit

--- textoi/src/components/About.js
+++ textoi/src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       This is About page
       <p>
-      ipsum dolor sit amet consectetur adipisicing elit. Ducimus, mollitia odio? Itaque eum minima eius dolor veritatis deleniti, officiis, placeat unde voluptatum corrupti culpa ducimus nemo omnis veniam labore quidem.
+            will  start to work on this soon
       </p>
       </div>
   )
